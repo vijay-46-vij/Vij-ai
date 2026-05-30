d236ce7fe063c7c319cb902de89e34402dac513c
vijay-46-vij <somuvijaysimhareddy@gmail.com>
2026-05-30 20:27:50 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VijAI from './VijAI.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><VijAI /></React.StrictMode>
+)
